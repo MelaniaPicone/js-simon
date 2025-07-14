b7e05adb8459b50551ebb10d461b9d56e4eeae03
MelaniaPicone <melania_picone@yahoo.it>
2025-07-14 10:49:51 +0200

    [in-class correction]

--- js/script.js
+++ js/script.js
@@ -1,38 +1,92 @@
 // recupero degli id
 const numList = document.getElementById ("numbers-list");
-const countdown = document.getElementById("countdown");
+const countdownElement = document.getElementById("countdown");
 const form = document.getElementById("answers-form");
 const inputs = form.querySelectorAll("input");
 const message = document.getElementById("message");
+const instructions = document.getElementById('instructions');
+const button = document.querySelector('button');
 
 
-let randomNum = [];
+const generateRandomNumbers = (min, max) => {
+const randomNum = [];
+
+// ciclo fino a c he l'array random non abbia lunghezza 5
+while(randomNum.length<5){
+
+const number = Math.floor(Math.random() * (max - min +1) ) + min;
+
+if(randomNum.includes(number) === false) {
+  randomNum.push(number);
+} }
+
+return randomNum; }
+
 let time = 30;
 
-// ciclo i numeri casuali
-for (let i = 0; i < 5; i++) {
-  const ranNum = Math.floor(Math.random() * 50) + 1;
-  randomNum.push(ranNum);
-}
+const numbers = generateRandomNumbers(1,30);
 
 // ciclo i numeri su schermo
-for (let i = 0; i < randomNum.length; i++) {
-  const li = document.createElement("li");
-  li.textContent = randomNum[i];
-  numList.appendChild(li);
+for (let i = 0; i < numbers.length; i++) {
+numList.innerHTML += `<li class= "list-item"> ${numbers[i]}</li>`;
 }
 
-const timer = setInterval(() =>
-{  countdown.textContent = `Tempo: ${time} secondi`;
-  time --;
+// timer
+countdownElement.innerText = time;
 
-  if (time === 20){
-numList.innerHTML = "";
-form.classList.remove("d-none");
+const countdown = setInterval(() => {
+time --;
+countdownElement.innerText= time;
+
+if (time === 20) {
+clearInterval(countdown);
+form.classList.remove('d-none');
+numList.classList.add('d-none');
+instructions.innerText = 'Digita i numeri che ricordi e visualizza il risultato'
+
+}
+}, 1000);
+
+// al clic del pulsante mi recupera i numeri che l'utente ha inserito e controlla quali sono presenti nell'array random
+
+const confirm = () => {
+
+// array numeri utente
+const userNumbers = [];
+const correctAnswers = [];
+
+for (let i=0; i<inputs.length ; i++) {
+  // bonus validazione
+  const value = parseInt(inputs[i].value);
+
+if (isNan(value) === false && value >= 1 && value <= 30 && userNumbers.includes(value) === false) {
+
+userNumbers.push(value);
+
+}
+} 
+
+if (userNumbers.length !== 5) {
+  message.innerText = "Ci sono valori non validi o duplicati"
+  return;
 }
 
-  if (time < 0) {
-clearInterval(timer);
-  }
+for(let i=0 ; i<userNumbers.length ; i ++) {
 
-}, 1000)
+if (numbers.includes(userNumbers[i])) {
+  correctAnswers.push(userNumbers[i]);
+}
+
+// messaggio
+message.innerText = `Hai indovinato ${correctAnswers.lenght} (${correctAnswers})`;
+
+}
+}
+
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  confirm();
+} )
+
+
